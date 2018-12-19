4f1beeb42e517647bd1e4b57a0bfb2b5799ebe48
navidAdelpour <navidadelpour@gmail.com>
2018-12-19 15:00:39 +0330

    completing the heuristic function

--- 8Puzzle_and_8Queen_HillClimbing_search_algorithms/EightQueenProblem.py
+++ 8Puzzle_and_8Queen_HillClimbing_search_algorithms/EightQueenProblem.py
@@ -1,19 +1,22 @@
 import random
+import math
 
 class EightQueenProblem:
+    # queens = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0)]
     queens = []
-
+    queens_num = 4
     def __init__(self):
         self.generateRandomQueens()
+        print(self.queens)
 
     def generateRandomQueens(self):
-        for i in range(8):
+        for i in range(self.queens_num):
             exists = True
             while exists:
                 exists = False
 
                 # generate new state
-                state = (random.randrange(8), random.randrange(8))
+                state = (random.randrange(self.queens_num), random.randrange(self.queens_num))
 
                 # ignore the checking for the first queen
                 if(len(self.queens) == 0):
@@ -30,4 +33,55 @@ class EightQueenProblem:
                     self.queens.append(state)
 
 
-EightQueenProblem()
+    def attackedCellsHeuristic(self):
+        board = [[0 for i in range(self.queens_num)] for j in range(self.queens_num)]
+        for (x, y) in self.queens:
+
+            for i in range(self.queens_num):
+                board[i][y] = 1
+
+            for i in range(self.queens_num):
+                board[x][i] = 1
+
+            i, j = x, y
+            while i != 0 and j != 0:
+                i = i - 1
+                j = j - 1
+                board[i][j] = 1
+
+            i, j = x, y
+            while i != self.queens_num - 1 and j != self.queens_num - 1:
+                i = i + 1
+                j = j + 1
+                board[i][j] = 1
+
+            i, j = x, y
+            while i != 0 and j != self.queens_num - 1:
+                i = i - 1
+                j = j + 1
+                board[i][j] = 1
+
+            i, j = x, y
+            while i != self.queens_num - 1 and j != 0:
+                i = i + 1
+                j = j - 1
+                board[i][j] = 1
+
+        h = 0
+        for i in board:
+            for j in i:
+                h += j
+        return h
+
+# 0 x 0 0
+# 0 0 x 0
+# 0 0 0 1
+# 0 0 x 0
+
+# 0 1 0 0
+# 0 0 0 1
+# 1 0 0 0
+# 0 0 1 0
+
+p = EightQueenProblem()
+print(p.attackedCellsHeuristic())
