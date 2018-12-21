30b09fc6642cdb1e54edbcee2b953e68125365b4
navidAdelpour <navidadelpour@gmail.com>
2018-12-21 23:01:51 +0330

    modifing the simulatedAnnealing

--- 8Puzzle_and_8Queen_HillClimbing_search_algorithms/HillClimbing.py
+++ 8Puzzle_and_8Queen_HillClimbing_search_algorithms/HillClimbing.py
@@ -20,6 +20,7 @@ def hillClimbing(problem, hardrate):
             break
         current = (successor[0], successor[1], current[2] + successor[2])
 
+    # calculate statistics
     time_elapsed = time.time() - t0
     accuracy = 1 - float(answer[1]) / float(initialH) if initialH and answer[1] else 1
     isGoal = answer[0].isGoal()
@@ -28,30 +29,35 @@ def hillClimbing(problem, hardrate):
     return answer
 
 def simulatedAnnealing(problem, hardrate):
-    temperature = 1000
+    temperature = 10000
     coolingRate = .003
 
+    t0 = time.time()
     problem.generateRandomBoard(hardrate)
     initialH = problem.heuristic()
     current = (problem, initialH, [])
     answer = None
 
-    print(current[0].getState())
-
     while(temperature > 1):
+        answer = current
+        if(current[0].isGoal()):
+            break
+
         successors = current[0].getSuccessors()
         successor = successors[random.randrange(len(successors))]
 
         if(probability(current[1], successor[1], temperature) > random.uniform(0, 1)):
             current = (successor[0], successor[1], current[2] + successor[2])
 
-        if(current[0].isGoal()):
-            answer = current
-            break
-
         temperature = temperature - temperature * coolingRate
     
-    print(answer != None and answer[0].getState())
+    # calculate statistics
+    time_elapsed = time.time() - t0
+    accuracy = 1 - float(answer[1]) / float(initialH) if initialH and answer[1] else 1
+    isGoal = answer[0].isGoal()
+
+    answer = answer + (isGoal, accuracy, problem, time_elapsed)
+    return answer
 
 def probability(e1, e2, t):
     de = e2 - e1
@@ -60,13 +66,19 @@ def probability(e1, e2, t):
     else:
         return math.exp(de / t)
 
-def solve(testNum, problem, trace):
+def solve(testNum, problem, algorithm, trace):
     trueSum = 0
     overallAccuracy = 0
     overallTime = 0
     hardrate = 20
     for i in range (testNum):
-        solution, h, path, isGoal, accuracy, initialState, time_elapsed = hillClimbing(problem, hardrate)
+        if algorithm == 'hillClimging':
+            answer = hillClimbing(problem, hardrate)
+        elif algorithm == 'simulatedAnnealing':
+            answer = simulatedAnnealing(problem, hardrate)
+        else:
+            return
+        solution, h, path, isGoal, accuracy, initialState, time_elapsed = answer
         if(isGoal):
             trueSum += 1
         overallAccuracy += accuracy
@@ -81,6 +93,7 @@ def solve(testNum, problem, trace):
             print(str(int(time_elapsed * 1000)) + ' ms')
 
     print("------------------------------------------------")
+    print algorithm
     print problem.__class__
     print 'overall win: ' + str(int(float(trueSum) / float(testNum) * 100)) + "%"
     print 'overall accuracy: ' + str(int(float(overallAccuracy) / float(testNum) * 100)) + "%"
@@ -88,15 +101,13 @@ def solve(testNum, problem, trace):
     print("------------------------------------------------")
 
 args = sys.argv
-if len(args) > 3:
+if len(args) > 4:
     if(args[2] == "NPuzzleProblem"):
         problem = NPuzzleProblem(int(args[3]))
     elif(args[2] == "NQueenProblem"):
         problem = NQueenProblem(int(args[3]))
 
-    solve(int(args[1]), problem, len(args) == 5 and args[4] == "--trace")
+    solve(int(args[1]), problem, args[4], len(args) == 6 and args[5] == "--trace")
 
-# solve(1, NPuzzleProblem(8), "--trace")
-# solve(1, NQueenProblem(4), "--trace")
-
-simulatedAnnealing(NPuzzleProblem(8), 10)
+# solve(1, NQueenProblem(4), "hillClimging", "--trace")
+# solve(1, NQueenProblem(4), "simulatedAnnealing","--trace")
